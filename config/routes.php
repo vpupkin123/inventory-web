83f37e88e89cc8c6b15e87a1a1556e7600d53498
vpupkin123 <zaikoz@yandex.ru>
2026-07-11 15:29:25 +0500

    Updated routes.php

--- config/routes.php
+++ config/routes.php
@@ -10,6 +10,11 @@ App::route('GET', '/logout', 'AuthController', 'logout');
 App::route('GET', '/change-password', 'AuthController', 'showChangePassword');
 App::route('POST', '/change-password', 'AuthController', 'changePassword');
 
+// Reports routes
+App::route('GET', '/reports', 'ReportController', 'index');
+App::route('GET', '/reports/upload', 'ReportController', 'showUpload');
+App::route('POST', '/reports/upload', 'ReportController', 'upload');
+
 // Dashboard (protected)
 App::route('GET', '/', 'DashboardController', 'index');
 App::route('GET', '/dashboard', 'DashboardController', 'index');
